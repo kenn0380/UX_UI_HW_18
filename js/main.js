810c64e5215d0b24d9f82866465d8a52dad7e23b
kenn0380 <kenn0380@umn.edu>
2021-11-07 17:01:16 -0600

    Case Study tile stylings

--- js/main.js
+++ js/main.js
@@ -60,3 +60,10 @@ $("#my-skills-tile-ui").hover(function() {
 });
 
 
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
